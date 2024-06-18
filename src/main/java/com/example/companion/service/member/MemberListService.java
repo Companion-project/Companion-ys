5757796fba1f7a797fef053332a3dfe5ee105359
DESKTOP-OIPA4EB\admin <lily.yang@outlook.kr>
2024-06-18 20:04:10 +0900

    memberList - paging, search

--- src/main/java/com/example/companion/service/member/MemberListService.java
+++ src/main/java/com/example/companion/service/member/MemberListService.java
@@ -3,6 +3,7 @@ package com.example.companion.service.member;
 import com.example.companion.domain.MemberDTO;
 import com.example.companion.domain.StartEndPageDTO;
 import com.example.companion.mapper.MemberMapper;
+import com.example.companion.service.StartEndPageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
@@ -15,30 +16,19 @@ public class MemberListService {
     @Autowired
     MemberMapper memberMapper;
 
+    @Autowired
+    StartEndPageService startEndPageService;
+    String searchWord;
+
     public void execute(Model model, String searchWord, int page){
 
-        int limit=10;
-        int startRow = ((page-1)*limit)+1;
-        int endRow = startRow + limit -1;
+        if(searchWord != null) {
+            this.searchWord = searchWord.trim();
+        }
 
-        StartEndPageDTO sepDTO = new StartEndPageDTO();
-        sepDTO.setEndRow(endRow);
-        sepDTO.setSearchWord(searchWord);
-        sepDTO.setStartRow(startRow);
+        StartEndPageDTO sepDTO = startEndPageService.execute(page, this.searchWord);
         List<MemberDTO> list = memberMapper.selectAll(sepDTO);
-        int count = memberMapper.memberCount(searchWord);
-        int limitPage = 10;
-        int startPage = (int)((double)page/limitPage+0.95-1)*limitPage+1;
-        int endPage = startPage+limitPage-1;
-        int maxPage = (int) ((double) count/limit+0.95);
-        if(maxPage < endPage) endPage = maxPage;
-
-        model.addAttribute("dtos", list);
-        model.addAttribute("searchWord", searchWord);
-        model.addAttribute("page", page);
-        model.addAttribute("startPage", startPage);
-        model.addAttribute("endPage", endPage);
-        model.addAttribute("count", count);
-        model.addAttribute("maxPage", maxPage);
+        int count = memberMapper.memberCount(this.searchWord);
+        startEndPageService.execute(page, count, model, list, this.searchWord);
     }
 }
