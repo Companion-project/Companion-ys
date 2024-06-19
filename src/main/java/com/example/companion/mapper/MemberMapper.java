fb64ed2834bfd9747b835c713103688decf06a06
Yunseo Lee <36lys20@gmail.com>
2024-06-19 09:38:22 +0900

    0619 등록일 에러 해결

--- src/main/java/com/example/companion/mapper/MemberMapper.java
+++ src/main/java/com/example/companion/mapper/MemberMapper.java
@@ -8,8 +8,9 @@ import java.util.List;
 
 @Mapper
 public interface MemberMapper {
+    //자바에서 메서드는 기본적으로 public이므로 생략했음.
     void memberInsert(MemberDTO dto);
     String memberAutoNum();
-    public List<MemberDTO> selectAll(StartEndPageDTO sepDTO);
-    public int memberCount(String searchWord);
+    List<MemberDTO> selectAll(StartEndPageDTO sepDTO);
+    int memberCount(String searchWord);
 }
