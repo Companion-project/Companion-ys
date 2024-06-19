6383c45145e2dc32c575ef15b2615979390bfb11
DESKTOP-OIPA4EB\admin <lily.yang@outlook.kr>
2024-06-19 23:24:19 +0900

    Email Authentication

--- src/main/java/com/example/companion/mapper/UserMapper.java
+++ src/main/java/com/example/companion/mapper/UserMapper.java
@@ -6,4 +6,5 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface  UserMapper {
     public int userInsert(MemberDTO dto);
+    public int userCheckUpdate(String email);
 }
--- src/main/java/com/example/companion/service/memberRegister/UserWriteService.java
+++ src/main/java/com/example/companion/service/memberRegister/UserWriteService.java
@@ -3,7 +3,9 @@ package com.example.companion.service.memberRegister;
 import com.example.companion.command.MemberCommand;
 import com.example.companion.domain.MemberDTO;
 import com.example.companion.mapper.UserMapper;
+import com.example.companion.service.EmailSendService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
@@ -16,6 +18,8 @@ public class UserWriteService {
     PasswordEncoder passwordEncoder;
     @Autowired
     UserMapper userMapper;
+    @Autowired
+    EmailSendService emailSendService;
 
     public void execute(MemberCommand memberCommand, Model model){
         String memberId = memberCommand.getMemberId();
@@ -42,7 +46,24 @@ public class UserWriteService {
         dto.setMemberPost(memberPost);
         dto.setMemberPw(passwordEncoder.encode(memberPw));//비밀번호 암호화
         dto.setMemberBirth(memberBirth);
+        int i = userMapper.userInsert(dto);
+        model.addAttribute("userName", dto.getMemberName());
+        model.addAttribute("userEmail", dto.getMemberEmail());
+        if(i>=1){
+            /// 메일링
+            String html= "<html><body>"
+                    + "    회원 가입을 축하합니다. <br />"
+                    + " 가입을 완료하시려면 "
+                    + "<a href='http://localhost:8089/userConfirm?chk=" + dto.getMemberEmail()
+                    + "'>여기</a>"
+                    + "를 눌러주세요";
+            String subject = "환영 인사입니다.";
+            String fromEmail = "markknow380@gmail.com"; // 보내는 사람
+            String toEmail = dto.getMemberEmail(); // 받는 사람
 
-        userMapper.userInsert(dto);
+            // 메일링 할 때마다 사용해야 하므로 모듈화시키겠습니다.
+            emailSendService.mailsend(html, subject, fromEmail, toEmail);
+            // 이메일이 전송이 될것이다. 이제 가입확인을 할 수 있는 userConfirm를 만들어야 한다.
+        }
     }
 }
